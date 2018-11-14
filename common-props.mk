20ab634d4a1f332cf06d237118b1428835dc65ab
TheScarastic <warabhishek@gmail.com>
2018-11-14 07:21:20 -0500

    Revert "sdm845-common: Disable framework ZSL as per stock"
    Zsl now works perfectly with latest MIUI builds
    
    This reverts commit 206df7a76772f4a135e4466246e7cb63e0fdf1ca.
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- common-props.mk
+++ common-props.mk
@@ -11,7 +11,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=true \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # CNE and DPM

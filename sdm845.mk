7f0d4b94ce827384c898f6aa1316306d9cf2e3fc
argraur <argraur@gmail.com>
2019-06-14 22:05:29 +0000

    sdm845-common: split framework-res
    Signed-off-by: argraur <argraur@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -302,6 +302,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_ENFORCE_RRO_TARGETS += \
+    framework-res
+
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \

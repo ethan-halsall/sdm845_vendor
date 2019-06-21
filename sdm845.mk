e36f81f613d234b9490080906b77fa3ec3b0a218
ethan-halsall <ethan.halsall@gmail.com>
2019-06-21 07:52:11 +0100

    Switch to power-libperfmgr
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -235,9 +235,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.sdm845
+    android.hardware.power@1.3-service.dipper-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI
 PRODUCT_COPY_FILES += \
